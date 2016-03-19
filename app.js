a77b50677bc882cb4e706772d07ce367a77f89a9
PLIROF <plirof@users.noreply.github.com>
2016-03-19 23:09:54 +0200

    Hardcoded sample code readonly lines*** v003
        	//++++++++++hard coded readonly lines from index.html++++++++++++++++++++++++++++++++
    		cm.markText({line:0,ch:0},{line:7,ch:0},{readOnly:true});//set read only lines 160319######
    		cm.markText({line:11,ch:0},{line:12,ch:0},{readOnly:true});//set read only lines 160319######
    		cm.markText({line:13,ch:0},{line:15,ch:0},{readOnly:true});//set read only lines 160319######
    		cm.markText({line:16,ch:0},{line:18,ch:0},{readOnly:true});//set read only lines 160319######
    		cm.markText({line:18,ch:0},{line:20,ch:0},{readOnly:true});//set read only lines 160319######
    		cm.markText({line:21,ch:0},{line:22,ch:0},{readOnly:true});//set read only lines 160319######
    		cm.markText({line:26,ch:0},{line:28,ch:0},{readOnly:true});//set read only lines 160319######
    		cm.markText({line:29,ch:0},{line:30,ch:30},{readOnly:true});//set read only lines 160319######      
            //----------------------------------hard coded readonly lines from index.html-------------------------

--- app.js
+++ app.js
@@ -82,7 +82,7 @@ var createEditor = function() {
 
     returnObj.getCodeObj = function() {
     	//++++++++++hard coded readonly lines from index.html++++++++++++++++++++++++++++++++
-		cm.markText({line:0,ch:0},{line:6,ch:0},{readOnly:true});//set read only lines 160319######
+		cm.markText({line:0,ch:0},{line:7,ch:0},{readOnly:true});//set read only lines 160319######
 		cm.markText({line:11,ch:0},{line:12,ch:0},{readOnly:true});//set read only lines 160319######
 		cm.markText({line:13,ch:0},{line:15,ch:0},{readOnly:true});//set read only lines 160319######
 		cm.markText({line:16,ch:0},{line:18,ch:0},{readOnly:true});//set read only lines 160319######
